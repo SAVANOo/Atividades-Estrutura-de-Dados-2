072e8a09fa306998de71997df367648f98180d37
Sávio Mendes <savio_gato1000@hotmail.com>
2023-10-18 23:08:19 -0300

    add 2 new activities

---
+++ Atividade 06 - Busca Binária Recursiva/Atividade 06.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define tam 5
+
+int buscaBinaria(int vet[], int key, int inicio, int fim);
+
+int main(){
+    int num[tam] = {89, 100, 120, 344, 567};
+    int chave;
+    printf("informe a chave de busca: ");
+    scanf("%d", &chave);
+    int final = tam - 1;
+    int result = buscaBinaria(num,chave,0,final);
+    if(result == -1){
+        printf("Chave nao encontrada");
+    } else {
+        printf("Chave encontrada na posicao %d", result);
+    }
+
+    return 0;
+}
+
+
+int buscaBinaria(int vet[], int key, int inicio, int fim){
+    int meio = (inicio+fim)/2;
+    if(fim >= inicio){
+        if(vet[meio] == key){
+        return meio;
+        } 
+        if(vet[meio] > key){
+            return buscaBinaria(vet,key,inicio, meio - 1);
+        } else {
+            return buscaBinaria(vet,key,meio + 1, fim);
+        }
+    }
+    return -1;
+}
